1a06d1a5c498608974a4ee0923f4d54413412113
Yehonal <yehonal.azeroth@gmail.com>
2016-10-10 16:37:48 +0200

    update with new structure

--- install.sh
+++ install.sh
@@ -17,4 +17,3 @@ if Joiner:with_dev ; then
     Joiner:add_repo "https://github.com/HW-Core/$NAME.git" "$NAME/tests" "tests" "$VENDOR"
     Joiner:add_repo "https://github.com/HW-Core/$NAME.git" "$NAME/doc" "gh-pages" "$VENDOR"
 fi
-
